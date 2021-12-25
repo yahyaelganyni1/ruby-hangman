f737f7b466dc3927418ff9921073f94e20a6a910
yahya <crazy20046@gmail.com>
2021-12-25 13:14:26 +0200

    Add geuss

--- play.rb
+++ play.rb
@@ -3,6 +3,13 @@ class Hangman
   def initialize
     @letters = ('a'..'z').to_a
     @word = words.sample
+    @lives = 7
+    @correct_guesses = []
+    @word_teaser = ""
+
+    @word.first.size.times do
+      @word_teaser += "_ "
+    end
   end
 
   def words
@@ -15,26 +22,47 @@ class Hangman
   ]
   end
 
-  def print_teaser
-    word_teaser = ""
+  def print_teaser last_guess = nil
+    update_teaser(last_guess) unless last_guess.nil?
+    puts @word_teaser
+  end
 
-    @word.first.size.times do
-    word_teaser += "_ "
+  def update_teaser last_guess 
+    new_teaser = @word_teaser.split
+    #replace blank values with letter if maches letter in word
+    new_teaser.each_with_index do |letter, index|
+      if letter == '_' && @word.first[index] == last_guess
+      new_teaser[index] = last_guess
+      end
     end
-
-    puts word_teaser
+    @word_teaser = new_teaser.join(' ')
   end
 
   def make_guess
-    puts "Enter a letter: "
-    guess = gets.chomp
-    # if letter is not part of the world then remove from letters array 
-    good_guess = @word.first.include? guess
+    if @lives > 0
+      puts "Enter a letter: "
+      guess = gets.chomp
+      # if letter is not part of the world then remove from letters array 
+      good_guess = @word.first.include? guess
 
-    if good_guess
-      puts "Good guess!"
+      if good_guess
+        puts "You are correct"
+
+        @correct_guesses << guess
+
+        #remove correct letter from the alphabet
+        @letters.delete guess
+
+        print_teaser guess
+        make_guess
+      else
+        puts "Sorry...try again"
+        @lives -= 1
+        puts "you have #{@lives} lives left. try again!"
+        make_guess
+      end
     else
-      puts "Sorry...try again"
+      puts "Game over!... Better luck next time!"
     end
   end
 
